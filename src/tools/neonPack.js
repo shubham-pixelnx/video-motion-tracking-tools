d2633e2a59fef723ec0757a5db9dd52991378196
Shubham Prajapat <shubham.prajapat@himanshusofttech.com>
2021-09-29 20:00:45 +0530

    added camshift tracking to neon pack tool

--- src/tools/neonPack.js
+++ src/tools/neonPack.js
@@ -49,16 +49,116 @@ class TrackingPopup extends Component {
 		this.ctx = null;
 	}
 	componentDidMount() {
-		this.videoTag.current.addEventListener("canplay", (e) => {
-			this.canvas = this.canvasTag.current;
-			this.canvas.width = this.videoTag.current.clientWidth;
-			this.canvas.height = this.videoTag.current.clientHeight;
+		(async () => {
+			this.props.showLoader();
+			let video = this.videoTag.current;
+			let blob = await fetch(video.src).then((e) => e.blob());
+			let url = URL.createObjectURL(blob);
+			video.src = url;
+			video.addEventListener("canplay", (e) => {
+				this.canvas = this.canvasTag.current;
+				video.width = video.clientWidth;
+				video.height = video.clientHeight;
+				this.canvas.width = video.clientWidth;
+				this.canvas.height = video.clientHeight;
 
-			this.initCanvas();
-		});
+				this.props.showLoader(false);
+				this.initCanvas();
+			});
+		})();
 	}
 	previewTracking = () => {
 		console.log(this.state.coords);
+		let video = this.videoTag.current;
+		let cv = window.cv;
+		let cap = new cv.VideoCapture(video);
+
+		// take first frame of the video
+		let frame = new cv.Mat(video.clientHeight, video.clientWidth, cv.CV_8UC4);
+		cap.read(frame);
+
+		// hardcode the initial location of window
+		let trackWindow = new cv.Rect(this.state.coords.x, this.state.coords.y, this.state.coords.width, this.state.coords.height);
+
+		// set up the ROI for tracking
+		let roi = frame.roi(trackWindow);
+		let hsvRoi = new cv.Mat();
+		cv.cvtColor(roi, hsvRoi, cv.COLOR_RGBA2RGB);
+		cv.cvtColor(hsvRoi, hsvRoi, cv.COLOR_RGB2HSV);
+		let mask = new cv.Mat();
+		let lowScalar = new cv.Scalar(30, 30, 0);
+		let highScalar = new cv.Scalar(180, 180, 180);
+		let low = new cv.Mat(hsvRoi.rows, hsvRoi.cols, hsvRoi.type(), lowScalar);
+		let high = new cv.Mat(hsvRoi.rows, hsvRoi.cols, hsvRoi.type(), highScalar);
+		cv.inRange(hsvRoi, low, high, mask);
+		let roiHist = new cv.Mat();
+		let hsvRoiVec = new cv.MatVector();
+		hsvRoiVec.push_back(hsvRoi);
+		cv.calcHist(hsvRoiVec, [0], mask, roiHist, [180], [0, 180]);
+		cv.normalize(roiHist, roiHist, 0, 255, cv.NORM_MINMAX);
+
+		// delete useless mats.
+		roi.delete();
+		hsvRoi.delete();
+		mask.delete();
+		low.delete();
+		high.delete();
+		hsvRoiVec.delete();
+
+		// Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+		let termCrit = new cv.TermCriteria(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1);
+
+		let hsv = new cv.Mat(video.clientHeight, video.clientWidth, cv.CV_8UC3);
+		let hsvVec = new cv.MatVector();
+		hsvVec.push_back(hsv);
+		let dst = new cv.Mat();
+		let trackBox = null;
+
+		const FPS = 30;
+		const processVideo = () => {
+			try {
+				if (/* video.paused || video.ended */ false) {
+					// clean and stop.
+					console.log("// clean and stop.");
+					frame.delete();
+					dst.delete();
+					hsvVec.delete();
+					roiHist.delete();
+					hsv.delete();
+					return;
+				}
+				let begin = Date.now();
+
+				// start processing.
+				cap.read(frame);
+				cv.cvtColor(frame, hsv, cv.COLOR_RGBA2RGB);
+				cv.cvtColor(hsv, hsv, cv.COLOR_RGB2HSV);
+				cv.calcBackProject(hsvVec, [0], roiHist, dst, [0, 180], 1);
+				// cv.imshow(this.canvas, dst);
+				// cv.imshow(this.canvas, dst);
+
+				// apply camshift to get the new location
+				[trackBox, trackWindow] = cv.CamShift(dst, trackWindow, termCrit);
+
+				// Draw it on image
+				let pts = cv.rotatedRectPoints(trackBox);
+				cv.line(frame, pts[0], pts[1], [255, 0, 0, 255], 3);
+				cv.line(frame, pts[1], pts[2], [255, 0, 0, 255], 3);
+				cv.line(frame, pts[2], pts[3], [255, 0, 0, 255], 3);
+				cv.line(frame, pts[3], pts[0], [255, 0, 0, 255], 3);
+				cv.imshow(this.canvas, frame);
+
+				// schedule the next one.
+				let delay = 1000 / FPS - (Date.now() - begin);
+				setTimeout(processVideo, delay);
+			} catch (err) {
+				console.log("err", err);
+			}
+		};
+
+		video.play();
+		// schedule the first one.
+		setTimeout(processVideo, 0);
 	};
 	getRelativeCursorPos = (element, event) => {
 		const elementRect = element.getBoundingClientRect();
@@ -190,10 +290,10 @@ class NeonPack extends Component {
 			await this.loadScript("/opencv.js");
 			// opencv.js loaded
 			window.cv.onRuntimeInitialized = () => {
+				this.props.showLoader(false);
 				this.setState((istat) => ({
 					trackingPopup: { ...istat.trackingPopup, open: true },
 				}));
-				this.props.showLoader(false);
 			};
 		})();
 	}
